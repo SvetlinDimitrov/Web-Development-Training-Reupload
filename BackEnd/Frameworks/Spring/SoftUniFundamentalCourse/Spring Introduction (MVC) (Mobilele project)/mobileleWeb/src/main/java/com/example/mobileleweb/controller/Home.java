362cc4961a67064cc1eae2b3520c4aef9fb45e80
Svetlin DImitrov <prolama6@gmail.com>
2023-06-09 15:08:21 +0300

    add to mobileleWeb project some practice

--- BackEnd/Frameworks/Spring/SoftUniFundamentalCourse/Spring Introduction (MVC) (Mobilele project)/mobileleWeb/src/main/java/com/example/mobileleweb/controller/Home.java
+++ BackEnd/Frameworks/Spring/SoftUniFundamentalCourse/Spring Introduction (MVC) (Mobilele project)/mobileleWeb/src/main/java/com/example/mobileleweb/controller/Home.java
@@ -1,7 +1,9 @@
 package com.example.mobileleweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,4 +16,5 @@ public class Home {
         modelAndView.setViewName("index");
         return modelAndView;
     }
+
 }
