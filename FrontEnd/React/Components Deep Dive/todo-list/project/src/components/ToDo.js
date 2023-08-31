a9d30aaa4115ccd7bde7e5af3148181c484764a7
Svetlin DImitrov <prolama6@gmail.com>
2023-08-31 06:08:40 +0300

    finish TODO list

--- FrontEnd/React/Components Deep Dive/todo-list/project/src/components/ToDo.js
+++ FrontEnd/React/Components Deep Dive/todo-list/project/src/components/ToDo.js
@@ -1,10 +1,10 @@
-export default function ToDo({text,isCompleted}){
+export default function ToDo({_id,text,isCompleted,changeButton}){
     return (
         <tr className= {isCompleted ? 'todo is-completed' : 'todo'}>
             <td>{text}</td>
-            <td> {isCompleted} ? Complete : Not Complete</td>
+            <td> {isCompleted ? 'Complete' : 'Not Complete'}</td>
             <td className="todo-action">
-              <button className="btn todo-btn">Change status</button>
+              <button id = {_id} onClick={() => changeButton(_id)} className="btn todo-btn">Change status</button>
             </td>
           </tr>
     )
