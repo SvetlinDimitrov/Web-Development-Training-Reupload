9c5efd89431ce600ec4ade1599b277cf562058b8
Светлин Димитров <dimitrovmail99@gmail.com>
2024-04-23 17:15:32 +0300

    add oauth2 server (not working)

---
+++ BackEnd/spring-security/oauth2_auth_server/src/main/java/org/demo/oauth2_auth_server/Oauth2AuthServerApplication.java
@@ -0,0 +1,13 @@
+package org.demo.oauth2_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2AuthServerApplication.class, args);
+  }
+
+}
---
+++ BackEnd/spring-security/oauth2_auth_server/src/test/java/org/demo/oauth2_auth_server/Oauth2AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.oauth2_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AuthServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
