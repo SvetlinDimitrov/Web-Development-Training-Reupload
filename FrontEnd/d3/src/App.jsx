6a95b458a243746f3616f6cbd4a564e11fa3a238
Светлин Димитров <dimitrovmail99@gmail.com>
2024-05-23 15:35:16 +0300

    finish tree

--- FrontEnd/d3/src/App.jsx
+++ FrontEnd/d3/src/App.jsx
@@ -7,26 +7,23 @@ function App() {
         {
             name: 'A',
             level: 0,
-            children: ['AD1', 'AD2'],
-            partner: 'D'
+            children: ['AB1', 'AB2'],
+            partner: 'B'
         },
         {
-            name: 'A1',
+            name: 'C',
             level: 0,
-            children: ['A1D11', 'A1D12', 'A1D13'],
-            partner: 'D1'
+            children: ['CD1', 'CD2', 'CD3'],
+            partner: 'D'
         },
-        {name: 'AD1', level: 1, partner: 'D0', children: ['AD1D01', 'AD1D02']},
-        {name: 'AD2', level: 1, partner: 'D1', children: ['AD2D11', 'AD2D12']},
-        {name: 'A1D11', level: 1, partner: 'D2'},
-        {name: 'A1D12', level: 1},
-        {name: 'A1D13', level: 1},
-        {name: 'AD1D01', level: 2},
-        {name: 'AD1D02', level: 2},
-        {name: 'AD2D11', level: 2, partner: 'K', children: ['AD2D11K1', 'AD2D11K2']},
-        {name: 'AD2D12', level: 2},
-        {name: 'AD2D11K1', level: 3},
-        {name: 'AD2D11K2', level: 3},
+        {name : 'AB1', level: 1 , partner: 'CD1' , children: ['AB1CD1' , 'AB1CD2' , 'AB1CD3']},
+        {name : 'AB2', level: 1 , partner: 'E' },
+        {name : 'CD1', level: 1},
+        {name : 'CD2', level: 1},
+        {name : 'CD3', level: 1},
+        {name : 'AB1CD1', level: 2},
+        {name : 'AB1CD2', level: 2},
+        {name : 'AB1CD3', level: 2},
     ];
     return <D3Component data={data1}/>;
 }
--- FrontEnd/d3/src/D3Component.jsx
+++ FrontEnd/d3/src/D3Component.jsx
@@ -18,18 +18,18 @@ function addZooming(svg, g) {
 function calculateAvailableCoordinates(data, dimensions, rectSizeX, rectSizeY, gapSize) {
 
     const levels = [...new Set(data.map(d => d.level))];
-    let currentY = 120;
+    let currentY = rectSizeY + gapSize;
     const coordinates = {};
 
     let totalPartners = 0;
     levels.forEach(level => {
 
         const startX = 0;
-        const rowData = data.filter(d => d.level === level-1);
+        const rowData = data.filter(d => d.level === level - 1);
         totalPartners += rowData.reduce((acc, curr) => curr.partner ? acc + 1 : acc, 0);
         let y = currentY * level;
 
-        if(totalPartners !== 0) {
+        if (totalPartners !== 0) {
             y += (rectSizeY + gapSize) * totalPartners / 2;
         }
 
@@ -43,8 +43,28 @@ function calculateAvailableCoordinates(data, dimensions, rectSizeX, rectSizeY, g
     return coordinates;
 }
 
-function drawNextRectangle(level, g, coordinates, rectSizeX, rectSizeY, gapSize, node, drawnNames, data) {
+function drawRectangle(g, coordinates, level, rectSizeX, rectSizeY, name, drawnNames, drawnRectCoordinates) {
+    g.append('rect')
+        .attr('x', coordinates[level].x)
+        .attr('y', coordinates[level].y)
+        .attr('width', rectSizeX)
+        .attr('height', rectSizeY)
+        .attr('fill', 'white')
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
 
+    g.append('text')
+        .attr('x', coordinates[level].x + rectSizeX / 2)
+        .attr('y', coordinates[level].y + rectSizeY / 2)
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'middle')
+        .text(name);
+
+    drawnNames.add(name);
+    drawnRectCoordinates[name] = {x: coordinates[level].x, y: coordinates[level].y};
+}
+
+function drawNextRectangle(level, g, coordinates, rectSizeX, rectSizeY, gapSize, node, drawnNames, drawnRectCoordinates, data, drawnLines) {
     const name = node.name;
     const partnerName = node.partner;
 
@@ -55,74 +75,110 @@ function drawNextRectangle(level, g, coordinates, rectSizeX, rectSizeY, gapSize,
     }
 
     if (!drawnNames.has(name)) {
-        g.append('rect')
-            .attr('x', coordinates[level].x)
-            .attr('y', coordinates[level].y)
-            .attr('width', rectSizeX)
-            .attr('height', rectSizeY)
-            .attr('fill', 'white')
-            .attr('stroke', 'black')
-            .attr('stroke-width', 1);
-
-        g.append('text')
-            .attr('x', coordinates[level].x + rectSizeX / 2)
-            .attr('y', coordinates[level].y + rectSizeY / 2)
-            .attr('text-anchor', 'middle')
-            .attr('dominant-baseline', 'middle')
-            .text(name);
-
-        drawnNames.add(name);
-        coordinates[level].x += rectSizeX + gapSize
+        drawRectangle(g, coordinates, level, rectSizeX, rectSizeY, name, drawnNames, drawnRectCoordinates);
+        coordinates[level].x += rectSizeX + gapSize;
     }
 
     if (partnerName && !drawnNames.has(partnerName)) {
-        g.append('rect')
-            .attr('x', coordinates[level].x)
-            .attr('y', coordinates[level].y)
-            .attr('width', rectSizeX)
-            .attr('height', rectSizeY)
-            .attr('fill', 'white')
-            .attr('stroke', 'black')
-            .attr('stroke-width', 1);
-
-        g.append('text')
-            .attr('x', coordinates[level].x + rectSizeX / 2)
-            .attr('y', coordinates[level].y + rectSizeY / 2)
-            .attr('text-anchor', 'middle')
-            .attr('dominant-baseline', 'middle')
-            .text(partnerName);
-
-        drawnNames.add(partnerName);
-        coordinates[level].x += rectSizeX * 2 + gapSize
+        drawRectangle(g, coordinates, level, rectSizeX, rectSizeY, partnerName, drawnNames, drawnRectCoordinates);
+        coordinates[level].x += rectSizeX * 2 + gapSize;
     }
 
     if (node && node.children && node.children.length > 0) {
         node.children.forEach(linkedName => {
             const linkedNode = data.find(d => d.name === linkedName);
             if (linkedNode) {
-                g.append('rect')
-                    .attr('x', coordinates[linkedNode.level].x)
-                    .attr('y', coordinates[linkedNode.level].y)
-                    .attr('width', rectSizeX)
-                    .attr('height', rectSizeY)
-                    .attr('fill', 'white')
-                    .attr('stroke', 'black')
-                    .attr('stroke-width', 1);
-
-                g.append('text')
-                    .attr('x', coordinates[linkedNode.level].x + rectSizeX / 2)
-                    .attr('y', coordinates[linkedNode.level].y + rectSizeY / 2)
-                    .attr('text-anchor', 'middle')
-                    .attr('dominant-baseline', 'middle')
-                    .text(linkedNode.name);
+                drawRectangle(g, coordinates, linkedNode.level, rectSizeX, rectSizeY, linkedNode.name, drawnNames, drawnRectCoordinates);
+                coordinates[linkedNode.level].x += rectSizeX + gapSize;
             }
-            drawnNames.add(linkedNode.name);
-            coordinates[linkedNode.level].x += rectSizeX + gapSize
-
         });
 
-        coordinates[level + 1].x += rectSizeX + gapSize
+        coordinates[level + 1].x += rectSizeX + gapSize;
     }
+
+    if (partnerName) {
+        const rowData = data.filter(d => d.level === level);
+        const totalLines = rowData.reduce((acc, curr) => curr.partner ? acc + 1 : acc, 0);
+        const totalLinesDrawn = drawnLines[level] || 0;
+
+        let currentY = coordinates[level].y + rectSizeY;
+        let nextY = coordinates[level + 1] ? coordinates[level + 1].y : currentY;
+        let gap = nextY - currentY;
+        let availableSpace = gap - 2 * rectSizeY; // Subtract the height of the rectangles at both levels
+        let distancePerLine = availableSpace / totalLines; // Use the entire available space
+        let lineY = currentY + rectSizeY + distancePerLine * totalLinesDrawn;
+
+        let xCoordinates = [];
+
+        xCoordinates.push(drawnRectCoordinates[name].x + rectSizeX / 2);
+
+        if (node.partner) {
+            xCoordinates.push(drawnRectCoordinates[node.partner].x + rectSizeX / 2);
+        }
+
+        if (node.children && node.children.length > 0) {
+            node.children.forEach(childName => {
+                const childNode = data.find(d => d.name === childName);
+                if (childNode) {
+                    xCoordinates.push(drawnRectCoordinates[childName].x + rectSizeX / 2);
+                }
+            });
+        }
+
+        const minX = Math.min(...xCoordinates);
+        const maxX = Math.max(...xCoordinates);
+
+        g.append('line')
+            .attr('x1', minX)
+            .attr('y1', lineY)
+            .attr('x2', maxX)
+            .attr('y2', lineY)
+            .attr('stroke', 'black')
+            .attr('stroke-width', 1);
+
+        drawnLines[level] = (drawnLines[level] || 0) + 1;
+
+        drawVerticalLine(g, name, lineY, rectSizeY, rectSizeX, drawnRectCoordinates);
+
+        if (node.partner) {
+            drawVerticalLine(g, node.partner, lineY, rectSizeY, rectSizeX, drawnRectCoordinates);
+        }
+        if (node.children && node.children.length > 0) {
+            node.children.forEach(childName => {
+                const childNode = data.find(d => d.name === childName);
+                if (childNode) {
+                    drawVerticalLine(g, childName, lineY, 0, rectSizeX, drawnRectCoordinates);
+                }
+            });
+        }
+
+        drawnLines[name] = {x1: minX, y1: lineY, x2: maxX, y2: lineY};
+
+        if (node.partner) {
+            drawnLines[node.partner] = {x1: minX, y1: lineY, x2: maxX, y2: lineY};
+        }
+        if (node.children && node.children.length > 0) {
+            node.children.forEach(childName => {
+                const childNode = data.find(d => d.name === childName);
+                if (childNode) {
+                    drawnLines[childName] = {x1: minX, y1: lineY, x2: maxX, y2: lineY};
+                }
+            });
+        }
+    }
+}
+
+function drawVerticalLine(g, nodeName, lineY, rectSizeY, rectSizeX, drawnRectCoordinates) {
+    const nodeCenterX = drawnRectCoordinates[nodeName].x + rectSizeX / 2;
+    const nodeBottomY = drawnRectCoordinates[nodeName].y + rectSizeY;
+
+    g.append('line')
+        .attr('x1', nodeCenterX)
+        .attr('y1', nodeBottomY)
+        .attr('x2', nodeCenterX)
+        .attr('y2', lineY)
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
 }
 
 function D3Component({data}) {
@@ -166,9 +222,11 @@ function D3Component({data}) {
         const coordinates = calculateAvailableCoordinates(data, dimensions, rectSizeX, rectSizeY, gapSize);
 
         const drawnNames = new Set();
+        const drawnRectCoordinates = {};
+        const drawnLines = {};
 
         data.forEach(node => {
-            drawNextRectangle(node.level, g, coordinates, rectSizeX, rectSizeY, gapSize, node, drawnNames, data);
+            drawNextRectangle(node.level, g, coordinates, rectSizeX, rectSizeY, gapSize, node, drawnNames, drawnRectCoordinates, data, drawnLines);
         });
     }, [data, dimensions]);
 
